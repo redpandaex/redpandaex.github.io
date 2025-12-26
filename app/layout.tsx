cb2010dc124de9f7a60f59df59a1ebe898ac50e1
redpandaex <lxw.tech.dev@gmail.com>
2025-12-26 23:59:13 +0800

    fix: update gsc

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
     template: `%s | ${siteConfig.name}`,
   },
   verification: {
-    google: "tBRpKoapUXsQYxUQfQg-_sr3QJjFWptBSQKQCTJsCRY",
+    google: "jZIW_JJilnDkihlbOvJH_2viVeLvwWXCACY_agwifhU",
     other: {
       "msvalidate.01": "251506A8C8052BFBD12CD6A66D49B33A",
     },
